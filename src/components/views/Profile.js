5f75146aac14712f0ff7c468c0d1f0abfe013ff7
Chiara Lentsch <lentsch.chiara@gmail.com>
2023-04-22 21:31:01 +0200

    Implement editable list

--- src/components/views/Profile.js
+++ src/components/views/Profile.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState, useMemo } from "react";
 import "styles/views/Profile.scss";
 import BaseContainer from "components/ui/BaseContainer";
-import { useHistory, useParams } from "react-router-dom";
+import ToDoList from "components/ui/List";
+import { useParams } from "react-router-dom";
 import { api, handleError } from "helpers/api";
-import { useForm } from "react-hook-form";
 import AppContainer from "components/ui/AppContainer";
 
 const InfoField = (props) => {
@@ -11,21 +11,17 @@ const InfoField = (props) => {
     <div className="profile field">
       <label className="profile titles">{props.label}</label>
 
-      {/* <div style={{ display: "flex", alignItems: "center" }}> */}
       <input
         className="profile input"
         placeholder="enter here.."
         value={props.value}
         onChange={(e) => props.onChange(e.target.value)}
       />
-      {/* </div> */}
     </div>
   );
 };
 
 const Profile = (props) => {
-  const history = useHistory();
-  const { register, handleSubmit } = useForm();
   const { userId } = useParams();
   const [user, setUser] = useState(null);
   //isEditable is a variable that is set to false by defalut and becomes true when the modify profile button is pressed
@@ -37,9 +33,6 @@ const Profile = (props) => {
   useEffect(() => {
     async function fetchData() {
       try {
-        // const response = await api.get("/users/" + userId, {
-        //   headers: { TOKEN: localStorage.getItem("token") },
-        // });
         const response = await api.get("/users/" + userId, { headers });
 
         // Get the returned users and update the state.
@@ -48,8 +41,7 @@ const Profile = (props) => {
         console.log(response);
       } catch (error) {
         console.error(
-          `Something went wrong while fetching the users: \n${
-            handleError(error).info
+          `Something went wrong while fetching the users: \n${handleError(error).info
           }`
         );
         console.error("Details:", error);
@@ -66,79 +58,83 @@ const Profile = (props) => {
     <AppContainer>
       <BaseContainer>
 
-          <div className="profile form">
-            <div className="profile main">
-              <i class="profile icon">account_circle</i>
-              {!isEditable && <div className="profile text"> myusername </div>}
-              {!isEditable && (
-                <div className="profile diet"> diet preference </div>
-              )}
-            </div>
-
+        <div className="profile form">
+          <div className="profile main">
+            <i className="profile icon">account_circle</i>
+            {!isEditable && <div className="profile text"> {user?.username} </div>}
             {!isEditable && (
-              <div className="profile sections">
-                <div className="profile preferences">
-                  <div className="profile titles">
-                    Allergies
-                    <ul id="list">
-                      <li data-new="true">
-                        <span>Peanuts</span>
-                        <input type="text" />
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-
-                <div className="profile preferences">
-                  <div className="profile titles">
-                    Favourite cuisine
-                    <ul id="list">
-                      <li data-new="true">
-                        <span>Italian</span>
-                        <input type="text" />
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
+              <div className="profile diet"> diet preference </div>
             )}
+          </div>
 
-            {isEditable && (
-              <div className="profile modify-section">
-                <InfoField label="Username" />
-                <InfoField label="Current Password" />
-                <InfoField label="Diet preference" />
-                <InfoField label="New Password" />
-
-                <div className="profile list">
-                  <div className="profile titles"> Allergies </div>
+          {!isEditable && (
+            <div className="profile sections">
+              <div className="profile preferences">
+                <div className="profile titles">
+                  Allergies
                   <ul id="list">
                     <li data-new="true">
-                      <span>add another</span>
-                      <input type="text" />
-                    </li>
-                  </ul>
-                </div>
-
-                <div className="profile list">
-                  <div className="profile titles">Favorite cuisine</div>
-                  <ul id="list">
-                    <li data-new="true">
-                      <span>add another</span>
+                      <span>Peanuts</span>
                       <input type="text" />
                     </li>
                   </ul>
                 </div>
               </div>
-            )}
 
-            <div className="profile buttons">
+              <div className="profile preferences">
+                <div className="profile titles">
+                  Favourite cuisine
+                  <ul id="list">
+                    <li data-new="true">
+                      <span>Italian</span>
+                      <input type="text" />
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          )}
+
+          {isEditable && (
+            <div className="profile modify-section">
+              <InfoField label="Username" />
+              <InfoField label="Current Password" />
+              <InfoField label="Diet preference" />
+              <InfoField label="New Password" />
+
+              <div className="profile list">
+                <div className="profile titles">Allergies</div>
+                {/* <ul id="list">
+                  <li data-new="true">
+                    <span>Peanuts</span>
+                    <input type="text" />
+                  </li>
+                </ul> */}
+                <ToDoList></ToDoList>
+              </div>
+
+
+              <div className="profile list">
+                <div className="profile titles">Favorite cuisine</div>
+                {/* <ul id="list">
+                  <li data-new="true">
+                    <span>add another</span>
+                    <input type="text" />
+                  </li>
+                </ul> */}
+                <ToDoList></ToDoList>
+              </div>
+            </div>
+          )}
+
+          {localStorage.getItem("userId") == userId &&
+            (<div className="profile buttons">
               {!isEditable && (
                 <button
                   className="profile general-button"
                   width="24%"
                   onClick={() => {
-                    setIsEditable(!isEditable);
+                    toggleEdit();
                   }}
                 >
                   Edit profile
@@ -150,7 +146,7 @@ const Profile = (props) => {
                   className="profile cancel-button"
                   width="24%"
                   onClick={() => {
-                    setIsEditable(!isEditable);
+                    toggleEdit();
                   }}
                 >
                   Cancel
@@ -162,19 +158,27 @@ const Profile = (props) => {
                   className="profile general-button"
                   width="24%"
                   onClick={() => {
-                    setIsEditable(!isEditable);
+                    toggleEdit();
                   }}
                 >
                   Save
                 </button>
               )}
             </div>
-          </div>
+            )
+          }
+        </div>
       </BaseContainer>
     </AppContainer>
   );
-};
 
+  function toggleEdit() {
+    setIsEditable(!isEditable);
+  }
+  function toggleEditList() {
+  }
+
+};
 /**
  * You can get access to the history object's properties via the withRouter.
  * withRouter will pass updated match, location, and history props to the wrapped component whenever it renders.
