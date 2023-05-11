edb8c480cc69e7804853e5df4fd412b74d0a3cff
KallPap <91315860+KallPap@users.noreply.github.com>
2023-05-11 16:40:38 +0200

    Update GroupFormingHost.js
    #97

--- src/components/views/GroupFormingHost.js
+++ src/components/views/GroupFormingHost.js
@@ -1,13 +1,10 @@
 import { useState, useEffect, useMemo } from "react";
 import { api, handleError } from "helpers/api";
-import { Spinner } from "components/ui/Spinner";
-import { Button } from "components/ui/Button";
 import { useHistory } from "react-router-dom";
 import BaseContainer from "components/ui/BaseContainer";
 import PropTypes from "prop-types";
 import "styles/views/GroupFormingHost.scss";
 import AppContainer from "components/ui/AppContainer";
-import Group from "models/Group";
 import { useParams } from "react-router-dom";
 import useGroupMembers from "hooks/useGroupMembers";
 
@@ -15,6 +12,7 @@ const GroupFormingHost = () => {
   const history = useHistory();
   const { groupId } = useParams();
   const { group, users } = useGroupMembers(groupId);
+  const [joinRequests, setJoinRequests] = useState([]);
   const headers = useMemo(() => {
     return { "X-Token": localStorage.getItem("token") };
   }, []);
@@ -23,6 +21,49 @@ const GroupFormingHost = () => {
       await api.delete(`/groups/${groupId}`, { headers });
 
       history.push("/game");
+    } catch (error) {
+      handleError(error);
+    }
+  };
+
+  const fetchRequests = async () => {
+    try {
+      const response = await api.get(`/groups/${groupId}/requests`, {
+        headers,
+      });
+      setJoinRequests(response.data);
+      fetchRequests();
+    } catch (error) {
+      handleError(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchRequests();
+  }, []);
+
+  const handleAcceptRequest = async (guestId) => {
+    try {
+      await api.put(
+        `/groups/${groupId}/requests/accept`,
+        { guestId },
+        { headers }
+      );
+      // Handle success or update the UI accordingly
+    } catch (error) {
+      handleError(error);
+    }
+  };
+
+  const handleRejectRequest = async (guestId) => {
+    try {
+      await api.put(
+        `/groups/${groupId}/requests/reject`,
+        { guestId },
+        { headers }
+      );
+      fetchRequests();
+      // Handle success or update the UI accordingly
     } catch (error) {
       handleError(error);
     }
@@ -64,10 +105,33 @@ const GroupFormingHost = () => {
                             <span className="groupforming player username">
                               {member.username}
                             </span>
-                            {/* <button className="material-icons reply-button">
-      done
-    </button> */}
-                            <button className="material-icons reply-button">
+                          </div>
+                        ))}
+
+                      {joinRequests &&
+                        joinRequests.map((joinRequest) => (
+                          <div
+                            className="groupforming group-join-request"
+                            key={joinRequest.id} // Assuming the join request has an `id` property
+                          >
+                            <span className="groupforming player username">
+                              {joinRequest.username}{" "}
+                              {/* Assuming the username is available */}
+                            </span>
+                            <button
+                              className="material-icons reply-button"
+                              onClick={() =>
+                                handleAcceptRequest(joinRequest.id)
+                              }
+                            >
+                              done
+                            </button>
+                            <button
+                              className="material-icons reply-button"
+                              onClick={() =>
+                                handleRejectRequest(joinRequest.id)
+                              }
+                            >
                               delete_outline
                             </button>
                           </div>
