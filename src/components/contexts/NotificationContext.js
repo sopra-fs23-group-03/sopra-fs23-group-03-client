9d9e74582dab11908f1c46ed5f9737c5c236f40d
KallPap <91315860+KallPap@users.noreply.github.com>
2023-05-18 21:20:04 +0200

    leaving event

--- src/components/contexts/NotificationContext.js
+++ src/components/contexts/NotificationContext.js
@@ -50,7 +50,7 @@ export const NotificationProvider = ({ children }) => {
 
     // Clear the interval on unmount
     return () => clearInterval(intervalId);
-  }, []);
+  }, [isLoggedIn]);
 
   return (
     <NotificationContext.Provider
