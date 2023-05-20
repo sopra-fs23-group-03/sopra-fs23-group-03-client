ff34f10c2dd61fe6b44f3b006eab4ad7308ca813
Chiara Lentsch <lentsch.chiara@gmail.com>
2023-05-20 18:14:26 +0200

    Revert "Update useGroupMembers.js"
    This reverts commit f796c0a703ebd4f689ce5345bb3d1c637741ac0c.

--- src/hooks/useGroupMembers.js
+++ src/hooks/useGroupMembers.js
@@ -1,3 +1,54 @@
+// import { useState, useEffect, useMemo } from "react";
+// import { api, handleError } from "helpers/api";
+
+// const useGroupMembers = (groupId) => {
+//   const headers = useMemo(() => {
+//     return { "X-Token": localStorage.getItem("token") };
+//   }, []);
+
+//   const [group, setGroup] = useState(null);
+//   const [users, setUsers] = useState(null);
+
+//   useEffect(() => {
+//     async function fetchData() {
+//       try {
+//         const groupResponse = await api.get(`/groups/${groupId}`, { headers });
+//         const membersResponse = await api.get(`/groups/${groupId}/guests`, {
+//           headers,
+//         });
+//         setGroup(groupResponse.data);
+//         setUsers(membersResponse.data || []);
+//       } catch (error) {
+//         //if useGroupMembers(groupId); returns an 404 error, then the group does not exist and the user should be redirected to the dashboard
+
+//         console.error(
+//           `Something went wrong while fetching the group and its members: \n${handleError(
+//             error
+//           )}`
+//         );
+//         console.error("Details:", error);
+//         alert(
+//           "Something went wrong while fetching the group and its members! See the console for details."
+//         );
+//       }
+//     }
+
+//     fetchData(); // Call once on component mount
+
+//     const intervalId = setInterval(() => {
+//       fetchData(); // Call again after the specified interval
+//     }, 5000); // Poll every 5 seconds
+
+//     return () => {
+//       clearInterval(intervalId); // Clear the interval on component unmount
+//     };
+//   }, []); // Only run once on component mount
+
+//   return { group, users };
+// };
+
+// export default useGroupMembers;
+
 import { useState, useEffect, useMemo } from "react";
 import { api, handleError } from "helpers/api";
 
@@ -35,7 +86,7 @@ const useGroupMembers = (groupId) => {
             )}`
           );
           console.error("Details:", error);
-          
+          );
         }
 
       }
