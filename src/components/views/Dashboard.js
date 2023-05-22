6f86d1dde68c0a6399929b4c5778f142ec13a33a
Chiara Lentsch <lentsch.chiara@gmail.com>
2023-05-22 18:46:43 +0200

    Add info in dashboard

--- src/components/views/Dashboard.js
+++ src/components/views/Dashboard.js
@@ -198,15 +198,16 @@ const Dashboard = () => {
       content = (
         <div className="game main-container">
           <div className=" game sidebar">
+            <div className="game welcome">CollaborEat helps to find a suitable dish for your next planned meal. Start with creating/joining a group or go solo.</div>
             <Button
-              className="game sidebar-buttons"
+              className="game sidebar-buttons create-group"
               onClick={() => history.push("/group-creation")}
             >
               <i className="material-icons">groups</i> &nbsp; Form Group &nbsp;
             </Button>
 
             <Button
-              className="game sidebar-buttons"
+              className="game sidebar-buttons solo"
               onClick={() => history.push("/solo")}
             >
               <i className="material-icons">person</i>
