c21ac909e67ad3c21e3383a8f5ea32a4494d09b3
Chiara Lentsch <lentsch.chiara@gmail.com>
2023-05-24 15:24:08 +0200

    Welcome message fixes + margin for users

--- src/components/views/Dashboard.js
+++ src/components/views/Dashboard.js
@@ -334,12 +334,16 @@ const Dashboard = () => {
           )}
           {showInstructions && (
             <ConfirmationModal
-              message=" CollaborEat helps to find a suitable dish for your next planned meal. 
-              Refine your profile and set your personal preferences. If you are all alone tonight, 
-              try our go solo option to get a recipe suggestion based on your profile. If you feel more sociable, 
-              you can start creating/joining a group with your friends. After you typed in the ingredients you want to contribute, 
-              you can rate them. Taking also into account the group’s allergies you will recieve the best fitting recipe. Have fun!"
-              onConfirm={() => setShowInstructions(false)}
+            message={
+              <div>
+                CollaborEat helps to find a suitable dish for your next planned meal.
+                Refine your profile and set your personal preferences.<br />
+                If you are all alone tonight, try our go solo option to get a recipe suggestion based on your profile.<br />
+                If you feel more sociable, you can start creating/joining a group with your friends. After you typed in the ingredients you want to contribute,
+                you can rate them. Taking also into account the group’s allergies you will then receive the best fitting recipe.<br />
+                Have fun!
+              </div>}
+            onConfirm={() => setShowInstructions(false)}
             />
           )}
         </div>
